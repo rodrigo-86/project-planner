3b447cb7235284f1c383501db17f2bd742f69e05
rodrigo-86 <rodrigosilveira1102@gmail.com>
2022-10-19 16:54:53 -0300

    json-server added

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,16 +2,16 @@
   <q-layout view="hHh lpR fFf">
 
     <q-header elevated class="bg-primary text-white" height-hint="98">
-      <q-toolbar>
+      <q-toolbar align="center">
         <q-toolbar-title>
           <q-avatar>
             <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
           </q-avatar>
-          Title
+          Project Planner
         </q-toolbar-title>
       </q-toolbar>
 
-      <q-tabs align="left">
+      <q-tabs align="center">
         <q-route-tab to="/page1" label="Page One" />
         <q-route-tab to="/page2" label="Page Two" />
         <q-route-tab to="/page3" label="Page Three" />
